fb8aef7df4f495f71b1f5b1db350bb92ed24f569
Alex <acaputo7@gatech.edu>
2022-02-15 19:45:53 -0500

    eh

--- HW6_5.py
+++ HW6_5.py
@@ -1,9 +1,19 @@
 import numpy as np
 import sklearn
+from sklearn import linear_model
 import math as m
 import matplotlib.pyplot as plt
 
-x = np.linspace(-1,1,100).T
-y_x = np.sin(m.pi*x)
+X = np.linspace(-1,1,100)
+y_x = np.sin(m.pi*X)
+print(X)
+# reg = linear_model.LinearRegression().fit(X, y_x)
 
-plt.plot(x,y_x)
+# y_pred = reg.predict([X])
+
+
+plt.plot(X, y_x)
+# plt.plot(X, y_pred)
+plt.show()
+
+
